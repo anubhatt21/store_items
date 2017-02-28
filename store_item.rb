edf6c1b83a16503ccd5a11dbb7686a649788d7be
Anuradha Bhatt <anudave21@gmail.com>
2017-02-27 20:59:26 -0600

    Add class

--- store_item.rb
+++ store_item.rb
@@ -9,6 +9,17 @@ puts "The #{fruit3[:name]} is #{fruit3[:color]} and costs #{fruit1[:price]}"
 
 puts 
 
+
+fruit1 = {name: "apple" , color: "red", price: 1}
+fruit2 = {name: "mandarin", color: "orange", price: 2}
+fruit3 = {name: "banana" , color: "yellow", price: 3}
+
+puts "The #{fruit1[:name]} is #{fruit1[:color]} and costs #{fruit1[:price]}" 
+puts "The #{fruit2[:name]} is #{fruit2[:color]} and costs #{fruit1[:price]}" 
+puts "The #{fruit3[:name]} is #{fruit3[:color]} and costs #{fruit1[:price]}" 
+
+puts
+
 # Using arrays
 
 fruit1 = ["apple" , "red", 1]
@@ -21,11 +32,41 @@ puts "The " + fruit3[0] + " is "  + fruit3[1] + " and costs " + fruit1[2].to_s
 
 puts 
 
-fruit1 = {name: "apple" , color: "red", price: 1}
-fruit2 = {name: "mandarin", color: "orange", price: 2}
-fruit3 = {name: "banana" , color: "yellow", price: 3}
+class Items
+  attr_reader :name, :color
+  attr_writer :price
 
-puts "The #{fruit1[:name]} is #{fruit1[:color]} and costs #{fruit1[:price]}" 
-puts "The #{fruit2[:name]} is #{fruit2[:color]} and costs #{fruit1[:price]}" 
-puts "The #{fruit3[:name]} is #{fruit3[:color]} and costs #{fruit1[:price]}" 
+  def initialize(name, color, price)
+    @name = name
+    @color = color
+    @price = price
+    
+    def control_pricing
+    @price = 2 + @price
+    end
+  
+  end 
+
+  def name
+    @name
+  end
+
+  def color
+    @color
+  end
+
+  def price
+    @price
+  end
+
+  
+end
+
+fruit1 = Items.new("apple", "red", 1)
+fruit2 = Items.new("mandarin", "orange", 2)
+fruit3 = Items.new("banana", "yellow", 3)
+puts fruit1.name
+puts fruit1.color
+puts fruit1.price
+fruit1.control_pricing
 
